0608e823c76fdaa7ed0680955e94b0af3fe650fd
dung nguyen <aplintell@gmail.com>
2024-04-24 16:45:05 +0700

    first commit

---
+++ src/ConsoleApp.java
@@ -0,0 +1,33 @@
+
+public class ConsoleApp {
+	public static void main(String[] args) {
+
+		// tao variable longEdge : gia tri la 5 . data type
+		// STRICT TYPE
+		// INTEGER
+		// declare
+		// assign value 5 for longEdge
+		// initialize
+		double longEdge = 5.123123123;
+		double shortEdge = 3;
+		double chuvi = 2 * (longEdge + shortEdge);
+		// tinh dien tich and print on CONSOLE
+		double dientich = longEdge * shortEdge;
+
+		// STRING  "abc aff"
+		System.out.println("Chu vi : " + chuvi);
+		System.out.println("Dien tich : " + dientich);
+		
+		
+		char charA = 'v';
+		String stringX = "This is a String with many characters";
+		
+		// XX BYTE XX này khá là TO 
+		System.out.println(stringX);
+		
+		// int, long
+		// char, String 
+		// float , double
+
+	}
+}
